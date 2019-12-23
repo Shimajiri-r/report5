29b48b0e9959a552f1a8cfa58ba09a5dd2939e4e
Ryunosuke SHIMAJIRI <e185403@ie.u-ryukyu.ac.jp>
2019-12-24 07:24:23 +0900

    step3/add document

--- src/main/java/jp/ac/uryukyu/ie/e185403/LivingThing.java
+++ src/main/java/jp/ac/uryukyu/ie/e185403/LivingThing.java
@@ -1,11 +1,28 @@
 package jp.ac.uryukyu.ie.e185403;
 
+/**
+ * LivingThingクラス。
+ *  String name;//名前
+ *  int hitPoint;//体力
+ *  int attack;//攻撃力
+ *  boolean dead;//生死の状態。true=死亡
+ * Created by e185403 on 2019/12/24
+ */
+
 public class LivingThing {
+
     private String name;
     private int hitPoint;
     private int attack;
     private boolean dead;
 
+    /**
+     * コンストラクタ。名前、体力、攻撃力、生死状態を設定する。
+     * @param name 名前
+     * @param maximumHP 体力
+     * @param attack 攻撃力
+     * @param dead 生死状態
+     */
     public LivingThing(String name, int maximumHP, int attack,boolean dead){
         setName(name);
         setHitPoint(maximumHP);
